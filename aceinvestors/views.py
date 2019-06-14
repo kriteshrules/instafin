57d06ebf749897c02a475abc4007890b231ad3ca
Kritesh Abhishek <kriteshabhishek@Kriteshs-MacBook-Air.local>
2019-06-14 18:38:55 +0530

    investor added

--- aceinvestors/views.py
+++ aceinvestors/views.py
@@ -3,27 +3,6 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import AceInvestor
 
-aceinvestors = [
-    {
-        'investor_name': 'Rakesh Jhunjhunwala',
-        'stock_no': '16',
-        'investor_summary': 'Cras sit amet nibh libero, in gravida nulla',
-        'profile_link': 'abx.yzp'
-    },
-    {
-        'investor_name': 'Mohnish Pabrai',
-        'stock_no': '9',
-        'investor_summary': 'Cras sit amet nibh libero, in gravida nulla',
-        'profile_link': 'abx.yzp'
-    },
-    {
-        'investor_name': 'Dolly Khanna',
-        'stock_no': '26',
-        'investor_summary': 'Cras sit amet nibh libero, in gravida nulla',
-        'profile_link': 'abx.yzp'
-    }
-]
-
 @login_required
 def home(request):
     context = {
