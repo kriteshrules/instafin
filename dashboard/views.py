3a4b53fd52f70c418c742e0838f7086ea59c72bf
Kritesh Abhishek <kriteshabhishek@Kriteshs-MacBook-Air.local>
2019-06-22 01:10:01 +0530

    News api changes made

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,7 +7,9 @@ def home(request):
     import json
     api_request = requests.get('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=96f2ee30b88e4e6fb678f5a4192edfab')
     api = json.loads(api_request.content)
-    return render(request, 'dashboard/dashboard.html', {'api': api})
+    context = {
+        'api': api,
+        'title': 'Dashboard'
+    }
+    return render(request, 'dashboard/dashboard.html', context)
 
-
-#'title': 'Dashboard'
