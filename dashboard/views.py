3b1a7c32fb772e2ce9080f96a98259540caabd86
Kritesh Abhishek <kriteshabhishek@Kriteshs-MacBook-Air.local>
2019-06-22 00:44:53 +0530

    dashboard news api integrated

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return render(request, 'dashboard/dashboard.html', {'title': 'Dashboard'})
+    import requests
+    import json
+    api_request = requests.get('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=96f2ee30b88e4e6fb678f5a4192edfab')
+    api = json.loads(api_request.content)
+    return render(request, 'dashboard/dashboard.html', {'api': api})
+
+
+#'title': 'Dashboard'
