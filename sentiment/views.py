becaeb09e3949e1a4d1c734c5ea6307c31e9826f
Kritesh Abhishek <kriteshabhishek@Kriteshs-MacBook-Air.local>
2019-07-10 14:02:05 +0530

    Google sentiment module updated- table replaced by news

--- sentiment/views.py
+++ sentiment/views.py
@@ -2,50 +2,7 @@ from django.shortcuts import render
 import tweepy
 from textblob import TextBlob
 from django.contrib.auth.decorators import login_required
-from bs4 import BeautifulSoup
-import requests
-
-
-class Analysis:
-    def __init__(self, term):
-        self.term = term
-        self.sentiment = 0
-        self.subjectivity = 0
-        self.pos_count = 0
-        self.neg_count = 0
-        self.neu_count = 0
-        self.gheadline_pos = []
-        self.gheadline_neu = []
-        self.gheadline_neg = []
-        self.urls = []
-        self.url = 'https://www.google.com/search?q=={0}&source=lnms&tbm=nws'.format(self.term)
-
-    def run(self):
-        response = requests.get(self.url)
-        #print(response.text)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        headline_results = soup.find_all('div', class_='st')
-        headline_url = soup.select('.r a')
-        for h in headline_results:
-            #print(h.text)
-            blob = TextBlob(h.get_text())
-            self.sentiment += blob.sentiment.polarity / len(headline_results)
-            self.subjectivity += blob.sentiment.subjectivity / len(headline_results)
-
-            if blob.sentiment.polarity< 0:
-                self.gheadline_neg.append(h.text)
-                self.neg_count += 1
-            elif blob.sentiment.polarity== 0:
-                self.gheadline_neu.append(h.text)
-                self.neu_count += 1
-            elif blob.sentiment.polarity> 0:
-                self.gheadline_pos.append(h.text)
-                self.pos_count += 1
-
-        for link in headline_url:
-            actual_link = link.get('href')
-            url = 'https://google.com' + actual_link
-            self.urls.append(url)
+from sentiment.googlesentiment import Analysis
 
 
 @login_required
@@ -120,7 +77,8 @@ def googlesentiment(request):
         "neg_count": analysis.neg_count,
         "neu_count": analysis.neu_count,
         "title": 'Google Sentiment',
-        'urls': analysis.urls
+        "urls": analysis.urls,
+        "mylist": analysis.mylist,
     }
     return render(request, 'sentiment/google_sentiment.html', context)
 
