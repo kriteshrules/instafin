29f33cd6c943a17e0456f22164fded4458ecc6f7
Kritesh Abhishek <kriteshabhishek@Kriteshs-MacBook-Air.local>
2019-07-11 13:32:46 +0530

    Idea box database setup

--- ideabox/views.py
+++ ideabox/views.py
@@ -16,12 +16,9 @@ def home(request):
 def zerodebt(request):
     symbol = ZeroDebt.objects.values_list('stock_name', flat=True)
     comment = ZeroDebt.objects.values_list('comment', flat=True)
-
     q = getQuotes(symbol)
     q.run()
-
     mylist = zip(symbol, q.stocknamelist, q.closepricelist, comment)
-
     context = {
         'zerodebt': ZeroDebt.objects.all(),
         'title': 'Zero debt stocks',
@@ -31,40 +28,70 @@ def zerodebt(request):
 
 
 def dividendchampions(request):
+    symbol = DividendChampions.objects.values_list('stock_name', flat=True)
+    comment = DividendChampions.objects.values_list('comment', flat=True)
+    q = getQuotes(symbol)
+    q.run()
+    mylist = zip(symbol, q.stocknamelist, q.closepricelist, comment)
     context = {
         'dividendchampions': DividendChampions.objects.all(),
-        'title': 'Dividend Champions'
+        'title': 'Dividend Champions',
+        'mylist': mylist,
     }
     return render(request, 'ideabox/dividend_champions.html', context)
 
 
 def highriskreward(request):
+    symbol = HighRiskReward.objects.values_list('stock_name', flat=True)
+    comment = HighRiskReward.objects.values_list('comment', flat=True)
+    q = getQuotes(symbol)
+    q.run()
+    mylist = zip(symbol, q.stocknamelist, q.closepricelist, comment)
     context = {
         'highriskreward': HighRiskReward.objects.all(),
-        'title': 'High Risk, High Reward'
+        'title': 'High Risk, High Reward',
+        'mylist': mylist,
     }
     return render(request, 'ideabox/highrisk_reward.html', context)
 
 
 def stockmonth(request):
+    symbol = StockMonth.objects.values_list('stock_name', flat=True)
+    comment = StockMonth.objects.values_list('comment', flat=True)
+    q = getQuotes(symbol)
+    q.run()
+    mylist = zip(symbol, q.stocknamelist, q.closepricelist, comment)
     context = {
         'stockmonth': StockMonth.objects.all(),
-        'title': 'Stock of the month'
+        'title': 'Stock of the month',
+        'mylist': mylist,
     }
     return render(request, 'ideabox/stock_month.html', context)
 
 
 def toplargecaps(request):
+    symbol = TopLargecaps.objects.values_list('stock_name', flat=True)
+    comment = TopLargecaps.objects.values_list('comment', flat=True)
+    q = getQuotes(symbol)
+    q.run()
+    mylist = zip(symbol, q.stocknamelist, q.closepricelist, comment)
     context = {
         'toplargecaps': TopLargecaps.objects.all(),
-        'title': 'Top 1% Largecap stocks'
+        'title': 'Top 1% Largecap stocks',
+        'mylist': mylist,
     }
     return render(request, 'ideabox/top_largecaps.html', context)
 
 
 def undervaluedstocks(request):
+    symbol = UndervaluedStocks.objects.values_list('stock_name', flat=True)
+    comment = UndervaluedStocks.objects.values_list('comment', flat=True)
+    q = getQuotes(symbol)
+    q.run()
+    mylist = zip(symbol, q.stocknamelist, q.closepricelist, comment)
     context = {
         'undervaluedstocks': UndervaluedStocks.objects.all(),
-        'title': 'Undervalued stocks'
+        'title': 'Undervalued stocks',
+        'mylist': mylist,
     }
     return render(request, 'ideabox/undervalued_stocks.html', context)
